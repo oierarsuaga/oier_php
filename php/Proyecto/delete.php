68c30e24ca91a934ce8735eb49aa92eb65072fec
AS3-2-04 <AS3-2-04@uni.lan>
2019-11-15 14:11:12 +0100

    a

---
+++ php/Proyecto/delete.php
@@ -0,0 +1,33 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//
+
+$sql = "SELECT * FROM blog";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. "<br> title: " . $row["title"]. "<br> info:" . $row["info"]. "<br> img: " . $row["img"];
+        ?>
+        <form action="delete.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $row["id"]?>">
+            <input type="submit" value="Delete">
+        </form>
+        <hr/>
+        <?php
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
