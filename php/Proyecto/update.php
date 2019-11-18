0d9f257f87e65949be57a83df8a49f67b6a557cf
AS3-2-04 <AS3-2-04@uni.lan>
2019-11-18 14:05:54 +0100

    a

--- php/Proyecto/update.php
+++ php/Proyecto/update.php
@@ -10,3 +10,19 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$title = $_POST['id'];
+$title = $_POST['title'];
+$info =  $_POST['info'];
+$img = $_POST['img'];
+
+$sql = "INSERT INTO blog (title, info, img) values('$title','$info','$img')";
+$result = $conn->query($sql);
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?>
