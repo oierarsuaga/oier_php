e2a895d536d5e051dcc32becba49b51c21082c6e
AS3-2-04 <AS3-2-04@uni.lan>
2019-11-20 08:47:44 +0100

    a

--- php/Proyecto/select.php
+++ php/Proyecto/select.php
@@ -21,7 +21,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-     echo "<br>  <br> title: " . $row["title"]. "<br> <img width='20%' src=imagenes/" . $row["img"] .">  <br> info: " . $row["info"];
+     echo "<br>  <h1>title</</h1>--><h1>" . $row["title"]. "</h1><br> <img width='33%' src=imagenes/" . $row["img"] .">  <br> info: " . $row["info"];
     }
 } else {
     echo "0 results";
