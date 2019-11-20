72ddc6a15f4cfefc8edc2e93721731be85cae3c0
AS3-2-04 <AS3-2-04@uni.lan>
2019-11-20 12:01:52 +0100

    a

--- php/Proyecto/delete.php
+++ php/Proyecto/delete.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style>
+    a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
+    </style>
+  </head>
+
 <?php
 
 $servername = "localhost";
