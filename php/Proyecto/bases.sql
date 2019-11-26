01b9462f56504d07f4b95bf120fb5a898753dec3
AS3-2-04 <AS3-2-04@uni.lan>
2019-11-26 08:50:34 +0100

    a

---
+++ php/Proyecto/bases.sql
@@ -0,0 +1,13 @@
+create database login;
+
+create table datos(
+  id int auto_increment,
+  title varchar (100),
+  info varchar (250),
+  img varchar (100),
+);
+
+create table blog(
+  usuario varchar (50),
+  password varchar (50),
+);
