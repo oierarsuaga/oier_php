6ddf5b5f729e16144c0236bd827aa53077320936
AS3-2-04 <AS3-2-04@uni.lan>
2020-01-24 13:44:33 +0100

    a

--- workspace/_index.php
+++ workspace/_index.php
@@ -1,3 +1,4 @@
+<?php include "config.php";?>
 <!DOCTYPE html>https://www.youtube.com/watch?v=-bn51X9YYNs
 <html lang="en" dir="ltr">
   <head>
@@ -9,31 +10,37 @@
   <body>
 
     <?php
+    $consulta1 = mysqli_query($conexion, "SELECT * from comentarios")or die (mysqli_error($conexion));
+    while($row = mysqli_fetch_assoc($consulta1)){
+      echo "<div class='comentarios'>".$row['usuario']."<br><br>
+      ".$row['comentario']."
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "login";
-    // Create connection
-    $conn = new mysqli($servername, $username, $password,$dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+      </div>";
     }
-    //
-    $sql = "SELECT * FROM comentarios";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-         echo "<br>  usuario</-->" . $row["usuario"]. ">  <br><br> texto: " . $row["comentario"];
-        }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
 
      ?>
+
+     <certer><form action="" method="post">
+       <input type="text" name="usuario" placeholder="usuario"><br><br>
+       <input type="text" name="comentario" placeholder="comentario"><br><br>
+       <input type="submit" name="enviar" value="Enviar comentario">
+     </form></certer>
+
+     <?php
+     if (isset($_POST['enviar'])) {
+       $usuario = utf8_decode(mysqli_real_escape_string($conexion, $_POST['usuario']));
+       $comentario = utf8_decode(mysqli_real_escape_string($conexion, $_POST['comentario']));
+       if ($usuario == '' or $comentario == ''){
+         echo "Lo sentimos, pero no puede dejar los campos sin rellenar.";
+       }
+       else {
+         $insertar = mysqli_query($conexion, "INSERT INTO comentarios(usuario, comentario) VALUES('".$usuario."', '".$comentario."')")or die(mysqli_error($conexion));
+         echo "El comentario ha sido introducido correctamente.";
+       }
+     }
+
+
+      ?>
 
 
   </body>
