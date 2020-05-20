477f1649cc0bb9d2e5abab098b30f226ea25ba25
oierarsuaga <oierarsuaga@gmail.com>
2020-05-20 03:20:03 +0200

    a

--- Web_FCT/Web/admin_clientes.php
+++ Web_FCT/Web/admin_clientes.php
@@ -1,4 +1,7 @@
+<?php
 session_start();
-if (!isset($_SESSION['username'])) {
-    header('Location: '."login_signin.php");
+if (!isset($ldap_dn["username"])) {
+    header('Location: '."Login.php");
 }
+
+?>
