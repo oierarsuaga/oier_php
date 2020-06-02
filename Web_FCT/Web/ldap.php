560d05a84576ac3115a8fdd28839aca934c1c8f0
oierarsuaga <oierarsuaga@gmail.com>
2020-06-02 14:27:43 +0200

    a

--- Web_FCT/Web/ldap.php
+++ Web_FCT/Web/ldap.php
@@ -7,7 +7,7 @@
     ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
 
     if (@ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
-      header('Location:admin_clientes.php');
+      header('Location:home-.php');
       //echo "Authenticated";
     } else {
       echo "Invalid Credential";
